7a1e298bf00a87f7815ca453818946bf4973890d
Dan Kordik <dkordik.public@gmail.com>
2013-10-31 02:04:43 -0500

    Force unicode to stop things from blowing up with unicode character conversion

--- playerinfo.rb
+++ playerinfo.rb
@@ -3,9 +3,25 @@
 require 'eventmachine-distributed-notification'
 require 'json'
 
+class Hash
+  def to_utf8
+    Hash[
+      self.collect do |k, v|
+        if (v.respond_to?(:to_utf8))
+          [ k, v.to_utf8 ]
+        elsif (v.respond_to?(:encoding))
+          [ k, v.dup.force_encoding('UTF-8') ]
+        else
+          [ k, v ]
+        end
+      end
+    ]
+  end
+end
+
 class Watcher < EM::DistributedNotificationWatch
   def notify(name, user_info)
-	puts user_info.to_json
+	puts user_info.to_utf8.to_json
 	exit(0)
   end
 end
