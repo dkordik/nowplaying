0a8a02f00f94b07237f4f9a327538356b8d57f5c
Dan Kordik <dkordik.public@gmail.com>
2013-11-03 20:44:45 -0600

    Add Rdio support. Include 'source' property on nowplaying object to include per-player logic downstream

--- playerinfo.rb
+++ playerinfo.rb
@@ -19,14 +19,41 @@ class Hash
   end
 end
 
-class Watcher < EM::DistributedNotificationWatch
+class ITunesWatcher < EM::DistributedNotificationWatch
   def notify(name, user_info)
-	puts user_info.to_utf8.to_json
-	exit(0)
+    user_info["Source"] = "iTunes"
+    puts user_info.to_utf8.to_json
+    exit(0)
+  end
+end
+
+class SpotifyWatcher < EM::DistributedNotificationWatch
+  def notify(name, user_info)
+    user_info["Source"] = "Spotify"
+    puts user_info.to_utf8.to_json
+    exit(0)
+  end
+end
+
+class RdioWatcher < EM::DistributedNotificationWatch
+  def notify(name, ignore)
+    user_info = Hash.new
+    rdio_track = `osascript -e 'tell app "Rdio" to get properties of current track'`.strip
+    rdio_track.split(", ").each do |prop|
+      key, val = prop.split(":")
+      if key != "artwork" #ignore full artwork image data
+        user_info[key] = val
+      end
+    end
+    user_info["Player State"] = `osascript -e 'tell app "Rdio" to get player state'`.strip
+    user_info["Source"] = "Rdio"
+    puts user_info.to_utf8.to_json
+    exit(0)
   end
 end
 
 EM.run {
-  EM.watch_distributed_notification('com.apple.iTunes.playerInfo', Watcher)
-  EM.watch_distributed_notification('com.spotify.client.PlaybackStateChanged', Watcher)
+  EM.watch_distributed_notification('com.apple.iTunes.playerInfo', ITunesWatcher)
+  EM.watch_distributed_notification('com.spotify.client.PlaybackStateChanged', SpotifyWatcher)
+  EM.watch_distributed_notification('com.rdio.desktop.playStateChanged', RdioWatcher)
 }
