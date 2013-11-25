c40fc6c10f8df247ab0b884b3bc78c82190e5585
Dan Kordik <dkordik.public@gmail.com>
2013-11-25 00:03:35 -0600

    1.0.4 - handle multiple events coming from eventmachine at the same time

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ NowPlaying.prototype.init = function () {
 	instance.playerinfo.stdout.on("data", function (data) {
 		var buff = new Buffer(data);
 		var utf8string = buff.toString('utf8');
-		var obj = JSON.parse(utf8string);
+		var states = utf8string.replace("}{", "}\0{") .split("\0");
+		states.forEach(function (state) {
+			var obj = JSON.parse(state);
+			instance.emit(obj["playerState"].toLowerCase(), obj);
+		})
 
-		instance.emit(obj["playerState"].toLowerCase(), obj);
 	});
 	instance.playerinfo.stdout.on("end", function (data) {
 		instance.init();
